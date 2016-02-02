53c89c008e67409f9d3ace86e2efea90dc93d58a
SD <zswdzly@outlook.com>
2016-02-02 21:58:36 +0800

    send function

--- MisakaMisaka.js
+++ MisakaMisaka.js
@@ -3,8 +3,14 @@ var HOST = '127.0.0.1';
 var dgram = require('dgram');
 var message = new Buffer('Misaka Misaka is calling!!!');
 var client = dgram.createSocket('udp4');
-client.send(message, 0, message.length, PORT, HOST, function(err, bytes){
-  if(err) throw err;
-  console.log('Message sent to ' + HOST + ':'+ PORT);
-  client.close();
-});
+function send(words){
+  var host = '127.0.0.1';
+  var port = 2426;
+  var message = new Buffer(words);
+  client.send(message, 0, message.length, port, host, function(err, bytes){
+    if(err) throw err;
+    console.log('Message sent to ' + HOST + ':'+ PORT);
+    client.close();
+  });
+}
+send("This is Misaka Misaka~");
